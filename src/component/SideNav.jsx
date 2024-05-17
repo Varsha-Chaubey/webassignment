f33b046c5dcd748af8e5cc65e3b7707b6a489229
Varsha-Chaubey <varshachaubey63@gmail.com>
2024-05-17 09:01:02 +0530

    Added rest side nave items

--- src/component/SideNav.jsx
+++ src/component/SideNav.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import logo from '../assets/gift 1.png'; // replace with your logo path
-import homeIcon from '../assets/Home.png'; // replace with your home icon path
-import boxesIcon from '../assets/treasure-chest 1.png'; // replace with your boxes icon path
-import battlesIcon from '../assets/swords 1.png'; // replace with your battles icon path
-import vipIcon from '../assets/Crown.png';
-import affiliatesIcon from '../assets/User, People, Team Group.png'; // replace with your affiliates icon path
+import React from "react";
+import logo from "../assets/Layer 2.svg"; 
+import homeIcon from "../assets/home-icon.svg"; 
+import boxesIcon from "../assets/open-case-icon.svg"; 
+import battlesIcon from "../assets/battles-icon.svg"; 
+import vipIcon from "../assets/crown-icon.svg";
+import affiliatesIcon from "../assets/men-icon.svg"; 
+
+import vector1 from '../assets/Vector.svg'
+import vector2 from '../assets/Vector (1).svg'
+import vector3 from '../assets/Vector (2).svg'
+import vector4 from '../assets/Vector (3).svg'
+
+import twitter from '../assets/twitter.svg'
+import insta from '../assets/inst.svg'
+import tiktok from '../assets/tiktok.svg'
+import youtube from '../assets/youtube.svg'
+
+
 
 const SideNavigation = () => {
   return (
     <nav className="side-navigation">
-      <div className="side-nav-logo">
-        <img src={logo} alt="logo" className="logo" />
-      </div>
+     
       <div className="side-nav-links">
+      <div className="side-nav-item active">
+        <img src={logo} alt="logo" className="side-nav-icon"/>
+      </div>
         <div className="side-nav-item">
           <img src={homeIcon} alt="Home icon" className="side-nav-icon" />
         </div>
@@ -26,7 +39,41 @@ const SideNavigation = () => {
           <img src={vipIcon} alt="VIP icon" className="side-nav-icon" />
         </div>
         <div className="side-nav-item">
-          <img src={affiliatesIcon} alt="Affiliates icon" className="side-nav-icon" />
+          <img
+            src={affiliatesIcon}
+            alt="Affiliates icon"
+            className="side-nav-icon"
+          />
+        </div>
+      </div>
+
+      <div className="second-side-nav-links">
+        <div className="side-nav-item">
+          <img src={vector1} alt="Home icon" className="side-nav-icon" />
+        </div>
+        <div className="side-nav-item">
+          <img src={vector2} alt="Boxes icon" className="side-nav-icon" />
+        </div>
+        <div className="side-nav-item">
+          <img src={vector3} alt="Battles icon" className="side-nav-icon" />
+        </div>
+        <div className="side-nav-item">
+          <img src={vector4} alt="VIP icon" className="side-nav-icon" />
+        </div>
+      </div>
+
+      <div className="third-side-nav-links">
+        <div className="third-nav-item">
+          <img src={twitter} alt="Home icon" className="side-nav-icon" />
+        </div>
+        <div className="third-nav-item">
+          <img src={insta} alt="Boxes icon" className="side-nav-icon" />
+        </div>
+        <div className="third-nav-item">
+          <img src={tiktok} alt="Battles icon" className="side-nav-icon" />
+        </div>
+        <div className="third-nav-item">
+          <img src={youtube} alt="VIP icon" className="side-nav-icon" />
         </div>
       </div>
     </nav>
--- src/component/TopNav.jsx
+++ src/component/TopNav.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from '../assets/_Слой_1.png'; // replace with your logo path
-import homeIcon from '../assets/Home.png'; // replace with your home icon path
-import boxesIcon from '../assets/treasure-chest 1.png'; // replace with your boxes icon path
-import battlesIcon from '../assets/swords 1.png'; // replace with your battles icon path
-import vipIcon from '../assets/Crown.png';
-import affiliatesIcon from '../assets/User, People, Team Group.png'; // replace with your affiliates icon path
+import React from "react";
+import logo from "../assets/_Слой_1.png";
+import homeIcon from "../assets/home-icon.svg";
+import boxesIcon from "../assets/open-case-icon.svg";
+import battlesIcon from "../assets/battles-icon.svg";
+import vipIcon from "../assets/crown-icon.svg";
+import affiliatesIcon from "../assets/men-icon.svg";
+
+
 
 const TopNavigation = () => {
   return (
@@ -15,23 +17,37 @@ const TopNavigation = () => {
       <div className="nav-links">
         <div className="nav-item ">
           <img src={homeIcon} alt="Home icon" className="nav-icon" />
-          <a href="#home" class="nav-item-link active">Home</a>
+          <a href="#home" class="nav-item-link active">
+            Home
+          </a>
         </div>
         <div className="nav-item">
           <img src={boxesIcon} alt="Boxes icon" className="nav-icon" />
-          <a href="#boxes" className=' nav-item-link '>Boxes</a>
+          <a href="#boxes" className=" nav-item-link ">
+            Boxes
+          </a>
         </div>
         <div className="nav-item">
           <img src={battlesIcon} alt="Battles icon" className="nav-icon" />
-          <a href="#battles" className=' nav-item-link '>Battles</a>
+          <a href="#battles" className=" nav-item-link ">
+            Battles
+          </a>
         </div>
         <div className="nav-item">
           <img src={vipIcon} alt="VIP icon" className="nav-icon" />
-          <a href="#vip" className=' nav-item-link '>VIP Members</a>
+          <a href="#vip" className=" nav-item-link ">
+            VIP Members
+          </a>
         </div>
         <div className="nav-item">
-          <img src={affiliatesIcon} alt="Affiliates icon" className="nav-icon" />
-          <a href="#affiliates" className=' nav-item-link '>Affiliates</a>
+          <img
+            src={affiliatesIcon}
+            alt="Affiliates icon"
+            className="nav-icon"
+          />
+          <a href="#affiliates" className=" nav-item-link ">
+            Affiliates
+          </a>
         </div>
       </div>
     </nav>
